03dce55fdda6ff84f18b9f7a31c01eeebe172deb
Xdennizhu20X <dennislrj25@gmail.com>
2025-01-08 23:15:02 -0500

    Updates

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 79286, hash: '8e580117cee6fef13994f63d5f421671e7f02654d13b4beb9a551c929970dd90', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 78017, hash: 'd763ee3c3fb49db6f02cf3615a65629772dbaf9be1ada95e66eedb566581c541', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 79286, hash: '19f3dd5f856cb6430b299532c3ab9ab3be21558f7e4ed2ac02e8e74549185c80', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 78017, hash: '0a90445e6ef49295302e33103db2cce6b5cd97c8a05aa4a482dd37395eefa2c9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-GP2IQNUC.css': {size: 7793, hash: 'oYuIwrXMym4', text: () => import('./assets-chunks/styles-GP2IQNUC_css.mjs').then(m => m.default)}
   },
 };
